94763af66feb0ffb9f13e24d22df036a2378e8ff
Sarita1611 <189330253+Sarita1611@users.noreply.github.com>
2025-07-20 18:18:45 +0000

    conversion and compaarision

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -1,18 +1,18 @@
 let score="33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 //33=>33
 //"33abc"=>NaN
 //true=>1; false=>0
 let isloogedIn=1
 let booleanIsLoggedIn= Boolean(isloogedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1=>true
 //0=>false
@@ -21,5 +21,40 @@ console.log(booleanIsLoggedIn);
 
 let sumNumber=33
 let stringNumber=String(sumNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+//********************************operations***************************************
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**6);
+// console.log(2/5);
+// console.log(2%6);
+
+let str1="hello"
+let str2=' sarita'
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+console.log(+true);
+console.log(+"");
+let num1,num2,num3;
+num1=num2=num3=2+2 //bad coding
+let gameCounter=300
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
 
