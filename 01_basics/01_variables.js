2ff5f713bb989a4f4643ff9cbf3f35e691390872
Sarita1611 <189330253+Sarita1611@users.noreply.github.com>
2025-07-20 17:32:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=4566
+let accountEmail="saritayadav123@gmail.com"
+var accountPassword="12345"
+accountCity="kanina"
+let accountState
+// accountId=2 not allowed
+console.log(accountId);
+accountEmail="sarita56.com"
+accountPassword="2121"
+accountCity="jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var
+because of issues of scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all js code as newer version
+//alert(3+3) we are using nodejs not browser
+console.log(3+
+    3
+); // this is bad code readablility should be high
+
+let name="sarita"
+let age=23
+let isloggedin=false
+//primitive datatypes->
+//number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean=>true or false
+//null => standalone value
+//undefined=>
+// symbol => to find if a component is unique or not//////unique
+
+//object datatypes
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined); ///undefind
