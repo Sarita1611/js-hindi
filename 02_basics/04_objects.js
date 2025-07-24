5b3606d498ee335963c006fad002632e03f99e59
Sarita1611 <189330253+Sarita1611@users.noreply.github.com>
2025-07-24 12:39:42 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -44,3 +44,16 @@ users[1].email
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 // console.log(tinderUser.hasOwnProperty("isloggedIn"));
+
+////destructuring
+const course={
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor:"sarita"
+}
+const {courseinstructor}=course
+console.log(courseinstructor);
+
+{
+    
+}
