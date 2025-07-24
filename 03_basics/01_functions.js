2b7ac56599a334040b9820bd49cc58ec6cc4d17c
Sarita1611 <189330253+Sarita1611@users.noreply.github.com>
2025-07-24 13:56:02 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,7 +19,7 @@ function addtwonumber(number1,number2){
     
 }
 const result=addtwonumber(3,5)
-console.log("result:",result)
+// console.log("result:",result)
 
 function loginusermessage(username="sam"){
     if (username===undefined){
@@ -28,5 +28,32 @@ function loginusermessage(username="sam"){
     }
     return `${username} just logged in`
 }
-console.log(loginusermessage("sarita"))
-console.log(loginusermessage())
+// console.log(loginusermessage("sarita"))
+// console.log(loginusermessage())
+
+
+function calculatecartprice(...num1){
+    return num1
+}
+console.log(calculatecartprice(200,400,500));
+
+const user={
+    username:"sarita",
+    price:900
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleobject(user)
+handleobject({
+    username:"hitesh",
+    price:45
+
+})
+const mynewarray=[200,400,500]
+function returnsecondvalue(getArray){
+    return getArray[0]
+}
+console.log(returnsecondvalue(mynewarray))
+console.log(returnsecondvalue([500,466]))
